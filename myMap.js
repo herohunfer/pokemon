3d55add5df94d6a538950c6f868f79dff4e265c8
Yan Wang <herohunfer@gmail.com>
2016-10-22 19:24:07 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -25,7 +25,7 @@ function get_pokemon_layer_from_map_items(map_items) {
         pushpins.add(pushpin);
     }
     var layer = new Microsoft.Maps.Layer();
-    layer.push_back(pushpins);
+    layer.push(pushpins);
     return layer;
 }
 
