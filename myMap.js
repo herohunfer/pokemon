516e3ac86f7c0b32f35d11576ef154c902d011f4
Yan Wang <herohunfer@gmail.com>
2016-10-22 13:24:24 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
 var map;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Your Bing Maps Key'
+        credentials: 'AvKTWSjO1pshQ6xHC9YaVQlDX2wBRuTtNyjU9_GL91NiH5TRgWPlW3DJZU8c4VRd'
     });
 }
